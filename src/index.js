a529132f369a4c432a5653a105ce0c9cb64b3aa0
Risyandi94 <risyandi@gvmnetworks.com>
2018-07-22 21:11:06 +0700

    update index js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Nav from './component/navigation/navigation';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<Nav />, document.getElementById('navigation'));
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('content'));
