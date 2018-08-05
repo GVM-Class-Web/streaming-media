612b52f2348283516c8eae3fff92feb5ff7639a3
Andrea Ottemusu <maria.a.ottemusu@gvmnetworks.com>
2018-08-05 23:49:41 +0700

    movie list display

--- src/component/content/movieList.js
+++ src/component/content/movieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { API_KEY, API_BASE_URL } from '../defenition/app'
-import { Link } from 'react-router-dom'
-
+import { API_KEY, API_BASE_URL, IMAGE_BASE_URL } from '../defenition/app';
+import { Link } from 'react-router-dom';
+import '../../style/movieList.css';
 
 class movieList extends Component {
     constructor(props) {
@@ -12,7 +12,7 @@ class movieList extends Component {
     }
 
     componentDidMount() {
-        var requestUrl = API_BASE_URL + 'discover/movie?sort_by=popularity.desc&page=1&api_key=' + API_KEY
+        var requestUrl = API_BASE_URL + 'discover/movie?sort_by=popularity.desc&page=3&api_key=' + API_KEY
         fetch(requestUrl)
             .then((Response) => {
                 return Response.json()
@@ -26,25 +26,86 @@ class movieList extends Component {
 
     render() {
         const movieList = this.state.movieList;
-        return (
-            <div>
-                kalo movienya ini
-                {
-                    movieList.map(movie => {
-                        return (
-                            <div key={movie.id}>
-                                {movie.title}
-                                <Link to={`/movie/${movie.id}`} >
-                                    {movie.title}
-                                </Link>
-                            </div>
-                        )
 
-                    })
-                }
+        return (
+            <div className='container'>
+                <h1 className='list-title'>Popular of this Week</h1>
+                <div className='movie-popular'>
+                    {
+                        movieList.map((movie, i) => {
+                            var picture = IMAGE_BASE_URL + movie.backdrop_path,
+                                title = movie.title,
+                                rating = movie.vote_average,
+                                synopsis = movie.overview;
+                                if(synopsis.length > 160){
+                                    var overview = synopsis.substr(0,160) + ' ... ';
+                                } else {
+                                    var overview = synopsis;
+                                }
+                            if (i > 3 && i < 7) {
+                                return (
+                                    <div key={i} className='movie-wrapper'>
+                                        <img className='movie-pict' src={picture}/>
+                                        <div className='overlay-info-popular'>
+                                            <div className='movie-info-popular'>
+                                                {title}
+                                                <br/>{rating} / 10
+                                                <br/>{overview}    
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })
+                    }
+                </div>
+
+                <h1 className='list-title'>Trending Now</h1>
+                <div className='movie-trending'>
+                    {
+                        movieList.map((movie, i) => {
+                            var picture = IMAGE_BASE_URL + movie.backdrop_path,
+                                title = movie.title,
+                                rating = movie.vote_average,
+                                synopsis = movie.overview;
+                                if(synopsis.length > 160){
+                                    var overview = synopsis.substr(0,160) + ' ... ';
+                                } else {
+                                    var overview = synopsis;
+                                }
+                            if (i < 4) {
+                                return (
+                                    <div key={i} className='movie-wrapper'>
+                                        <img className='movie-pict' src={picture}/>
+                                        <div className='overlay-info-trending'>
+                                            <div className='movie-info-trending'>
+                                                {title}
+                                                <br/>{rating} / 10
+                                                <br/>{overview}    
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })
+                    }
+                </div>
             </div>
+
         );
     }
 }
+
+
+// class contentOverlay extends Component {
+//     render() {
+//         return (
+    // <h4 className='movie-title'>{title}</h4>
+    
+//             <div className='movie-rating'>{rating} / 10</div>
+//  <div className='movie-synopsis'>{overview}</div> }
+//         );
+//     }
+// }
 
 export default movieList;
